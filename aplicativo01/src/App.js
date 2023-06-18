513dda8b2930e060f8dda331bd985495b52cd6fc
dannielfisico <treinamentosinterativa@gmail.com>
2023-06-18 18:42:00 -0300

    primeiro app react

--- aplicativo01/src/App.js
+++ aplicativo01/src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+//#region inicio
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+//#endregion fim
+
+
+import React, { useState } from 'react'
+import './App.css'
+
+const App = () => {
+  // let mensagem = "Boa noite Daniel!"
+  let [mensagem, setMensagem] = useState("Boa noite Daniel")
+  return <>
+            <div className='container'>{mensagem}</div>
+            <button onClick={() => setMensagem("Guaraciaba do Norte")} >Mudar Mensagem</button>
+        </>
 }
 
 export default App;
