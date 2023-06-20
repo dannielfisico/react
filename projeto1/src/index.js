8a5735408ca037b814c4a7ec1b66a012fcae93ce
dannielfisico <treinamentosinterativa@gmail.com>
2023-06-20 02:42:51 -0300

    Curso WebModerno - Criando Primeiro Componente

---
+++ projeto1/src/index.js
@@ -0,0 +1,7 @@
+import React from  'react'
+import ReactDOM from  'react-dom'
+
+const elementoHTML  = <h1>Estou aprendendo REACT</h1>
+
+ReactDOM.render(elementoHTML, document.getElementById('root'))
+
