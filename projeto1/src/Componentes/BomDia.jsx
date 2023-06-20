35efb1241c5130efc2c2f65c1769042d1e7dbbfb
dannielfisico <treinamentosinterativa@gmail.com>
2023-06-20 08:59:07 -0300

    133 - Multiplos Componentes

--- projeto1/src/Componentes/BomDia.jsx
+++ projeto1/src/Componentes/BomDia.jsx
@@ -3,5 +3,8 @@ import React from "react";
 export default props => 
       <>
           <h1>Bom dia {props.nome}, você tem {props.idade} anos. Estudando: {props.estudando}</h1> 
-          <h2>Até mais tarde!</h2>
-      </>      
+          <h2>Até mais tarde!!!</h2>
+      </>   
+      
+      
+      //Aula 132 WebModerno
