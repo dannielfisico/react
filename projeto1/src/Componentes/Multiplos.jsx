85764fb97e811ebd81d45c32296ebbde084a45cf
dannielfisico <treinamentosinterativa@gmail.com>
2023-06-20 09:28:25 -0300

    133 - Multiplos Componentes - parte2

--- projeto1/src/Componentes/Multiplos.jsx
+++ projeto1/src/Componentes/Multiplos.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
 
-export const BoaTarde = props => <h1>Boa Tarde {props.nome}!</h1>
-export const BoaNoite = props => <h1>Boa Noite {props.nome}!</h1>
+export const BoaTarde = props => <h1>Lhe desejo uma boa tarde {props.nome}!</h1>
+export const BoaNoite = props => <h1>Tenha uma boa noite {props.nome}!</h1>
+
+export default {BoaTarde, BoaNoite}
--- projeto1/src/index.js
+++ projeto1/src/index.js
@@ -4,7 +4,13 @@ import ReactDOM from  'react-dom'
 import Primeiro from './Componentes/Primeiro'
 import BomDia from  './Componentes/BomDia'
 
+//Importar variáveis do arquivo Multiplos.jsx
+import Cumprimentar from './Componentes/Multiplos'
 
-
-ReactDOM.render(<BomDia nome="Daniel" idade={37} estudando={true}/>, document.getElementById('root'))
+ReactDOM.render(
+    <>
+        <Cumprimentar.BoaTarde nome="Daniel Alves"/>
+        <Cumprimentar.BoaNoite nome='Daniel Araújo'/>
+    </>
+    , document.getElementById('root'))
 
