f53cad0d538065241c17c23e16ca6e45aba4a5e0
dannielfisico <treinamentosinterativa@gmail.com>
2023-06-22 19:00:15 -0300

    Primeico componente criado

--- projeto1/src/App.js
+++ projeto1/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Esta é a branch jogo da velha.
+          Esta é a branch learn.
         </p>
         <a
           className="App-link"
